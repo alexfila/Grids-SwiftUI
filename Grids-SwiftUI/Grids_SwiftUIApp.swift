5aa004d2884f7b9baa9cd27145370c1dc656fd46
Alex Fila <fialex66@gmail.com>
2024-10-12 22:35:58 +0200

    Initial Commit

---
+++ Grids-SwiftUI/Grids_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grids_SwiftUIApp.swift
+//  Grids-SwiftUI
+//
+//  Created by Alex Fila on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Grids_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
